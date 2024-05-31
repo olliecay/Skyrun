8f704f500491240cebd34e6d2e966220edce7072
ollie <122303892+olliecay@users.noreply.github.com>
2024-05-31 16:32:28 +0100

    Initial commit

---
+++ src/main/java/com/olliecay/skyrun/SkyRunDataGenerator.java
@@ -0,0 +1,11 @@
+package com.olliecay.skyrun;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkyRunDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
